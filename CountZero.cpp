b4dcc084f51c216a523328a8e4828e7e5bc8d1d0
Piyush Rawat <39116754+Darkece@users.noreply.github.com>
2018-10-22 23:52:19 +0530

    factorial using recursion efficient programming

--- CountZero.cpp
+++ CountZero.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
+int factorial(int num);
 int main()
 {
     int num,count=0,fact=1,rem;
@@ -8,11 +8,8 @@ int main()
     cin>>num;
 
     //Factorial of that number..
-    for(int i=num;i>0;i--)
-    {
-        fact=fact*i;
-    }
-
+    
+fact=factorial(num);
     //Counting the number of zeros..
     while(fact>0)
     {
@@ -24,4 +21,11 @@ int main()
         fact/=10;
     }
     cout<<"The number of zeros are: "<<count;
+}
+int factorial(int num)
+{
+ if(num==0)
+ return 1;
+ if(num>=1)
+ return num*factorial(num-1);
 }
